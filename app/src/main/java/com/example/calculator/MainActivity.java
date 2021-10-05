c9d843a616232cbca9b9a0ff6de9e00beee48fd2
Elizaveta9 <katkova_lizochka@bk.ru>
2021-10-05 23:01:12 +0300

    Initial commit

---
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.calculator;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+    }
+
+    public void onClick(View v) {
+
+        EditText firstNumEditText = findViewById(R.id.fistNum);
+        EditText secondNumEditText = findViewById(R.id.secondNum);
+
+        try {
+            String stringNum1 = firstNumEditText.getText().toString();
+
+            String stringNum2 = secondNumEditText.getText().toString();
+
+            if (isInteger(stringNum1) && isInteger(stringNum2)) {
+
+                int firstNum = Integer.parseInt(stringNum1);
+                int secondNum = Integer.parseInt(stringNum2);
+
+                Intent intent = new Intent(this, SecondActivity.class);
+                intent.putExtra("firstNum", firstNum);
+                intent.putExtra("secondNum", secondNum);
+                startActivity(intent);
+            } else {
+                Toast.makeText(this, "Некорректные данные", Toast.LENGTH_LONG).show();
+            }
+        } catch (Exception e) {
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public boolean isInteger(String s) {
+        try {
+            Integer.parseInt(s);
+        } catch (NumberFormatException e) {
+            return false;
+        }
+        return true;
+    }
+}
---
+++ app/src/main/java/com/example/calculator/SecondActivity.java
@@ -0,0 +1,38 @@
+package com.example.calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+      try {
+        TextView textView = (TextView) findViewById(R.id.resoultView);
+
+
+        Integer firstNum = getIntent().getExtras().getInt("firstNum");
+        Integer secondNum = getIntent().getExtras().getInt("secondNum");
+
+        int sum = firstNum + secondNum;
+
+        textView.setText(firstNum.toString() + " + " + isSecondNumNeg(secondNum) + " = " + Integer.toString(sum));
+
+        } catch (Exception e) {
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public String isSecondNumNeg(Integer num) {
+        if (num < 0) {
+            return "(" + num.toString() + ")";
+        }
+        return num.toString();
+    }
+
+}
